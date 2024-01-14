f8edd65aa895817e30f47cd04389e1b008f1daf5
Windree <windree@dragonsoul.com>
2024-01-14 10:37:07 +0000

    feat: add separate disk ram temperature stats

--- orange-pi-stats.sh
+++ orange-pi-stats.sh
@@ -1,20 +1,37 @@
 #!/bin/env bash
 set -Eeuo pipefail
 source "$(dirname "${BASH_SOURCE[0]}")/modules/get_local_partitions.sh"
-source "$(dirname "${BASH_SOURCE[0]}")/modules/get_partition_usage.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/modules/get_filesystem_usage.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/modules/get_memory_usage.sh"
+
+disk=0
+ram=0
+temperature=0
+
+for arg in "$@"; do
+    case $arg in
+    "disk") disk=1 ;;
+    "ram") ram=1 ;;
+    "temperature") temperature=1 ;;
+    esac
+done
 
 function get_ext4() {
     df --output=target --type ext4 | tail -n +2
 }
 
-get_ext4 | while read partition; do
-    echo "Disk usage $partition: $(get_filesystem_usage "$partition")"
-done
+if [ $disk -eq 1 ]; then
+    get_ext4 | while read partition; do
+        echo "Disk usage $partition: $(get_filesystem_usage "$partition")"
+    done
+fi
 
-echo "Memory usage:" $(get_memory_usage)
+if [ $ram -eq 1 ]; then
+    echo "Memory usage:" $(get_memory_usage)
+fi
 
-echo "Temperature:"
-echo "CPU: $(sensors | grep -A3 -P '^cpu' | grep -P '^temp1:' | awk '{print $2}')"
-echo "HDD: $(smartctl --all /dev/sda | grep -F "Temperature_Celsius" | awk '{print "+"$10".0°C"}')"
+if [ $temperature -eq 1 ]; then
+    echo "Temperature:"
+    echo "CPU: $(sensors | grep -A3 -P '^cpu' | grep -P '^temp1:' | awk '{print $2}')"
+    echo "HDD: $(smartctl --all /dev/sda | grep -F "Temperature_Celsius" | awk '{print "+"$10".0°C"}')"
+fi
